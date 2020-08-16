939146fbab8d124914d7193cddb16d26b9313f3b
Matheus Mandotti <mathew.mfranco@gmail.com>
2020-08-16 18:23:58 -0300

    Implement hide password and change page

--- todo_mobx/lib/screens/login_screen.dart
+++ todo_mobx/lib/screens/login_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_mobx/flutter_mobx.dart';
+import 'package:mobx/mobx.dart';
 import 'package:todomobx/widgets/custom_icon_button.dart';
 import 'package:todomobx/widgets/custom_text_field.dart';
 
@@ -11,8 +12,41 @@ class LoginScreen extends StatefulWidget {
   _LoginScreenState createState() => _LoginScreenState();
 }
 
-class _LoginScreenState extends State < LoginScreen > {
+class _LoginScreenState extends State<LoginScreen> {
   LoginStore loginStore = LoginStore();
+
+
+// Serve para limpar a memoria, pois o reaction fica rodando para sempre
+ReactionDisposer disposer;
+
+  @override
+  void didChangeDependencies() {
+    super.didChangeDependencies();
+
+    // autorun( (_) {
+
+    //   if( loginStore.loggedIn ){
+    //      Navigator.of(context).pushReplacement(
+    //       MaterialPageRoute( builder: (context) => ListScreen() ) );
+    //   }
+
+    // });
+
+// duas func, primeira para monitorar um valor
+// a segunda é efeito, que recebe o valor modificado da primeira
+
+// a reaction diferenre do autorun nao olha para o valor na primeira vez que inicia,
+// ela espera uma troca do valor para poder executar
+    disposer = reaction( (_) => loginStore.loggedIn, (logged) {
+
+       if ( logged ) {
+         Navigator.of(context).pushReplacement(
+          MaterialPageRoute( builder: (context) => ListScreen() ) );
+       }
+    
+     }
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -20,68 +54,83 @@ class _LoginScreenState extends State < LoginScreen > {
       body: Container(
         alignment: Alignment.center,
         margin: const EdgeInsets.all(32),
-          child: Card(
+        child: Card(
             shape: RoundedRectangleBorder(
               borderRadius: BorderRadius.circular(16),
             ),
             elevation: 16,
             child: Padding(
               padding: const EdgeInsets.all(16),
-                child: Column(
-                  mainAxisSize: MainAxisSize.min,
-                  children: < Widget > [
-                    CustomTextField(
+              child: Column(
+                mainAxisSize: MainAxisSize.min,
+                children: <Widget>[
+                  
+                  Observer( builder: (_) {
+                    return CustomTextField(
                       hint: 'E-mail',
                       prefix: Icon(Icons.account_circle),
                       textInputType: TextInputType.emailAddress,
                       onChanged: loginStore.setEmail,
-                      enabled: true,
-                    ),
-                    const SizedBox(
-                        height: 16,
-                      ),
-                      CustomTextField(
+                      enabled: !loginStore.loading,
+                    );
+                  }
+                  ),
+
+                  const SizedBox(
+                    height: 16,
+                  ),
+
+                  Observer(
+                    builder: (_) {
+                      return CustomTextField(
                         hint: 'Senha',
                         prefix: Icon(Icons.lock),
-                        obscure: true,
+                        obscure: !loginStore.passwordVisible,
                         onChanged: loginStore.setPassword,
-                        enabled: true,
+                        enabled: !loginStore.loading,
                         suffix: CustomIconButton(
                           radius: 32,
-                          iconData: Icons.visibility,
-                          onTap: () {},
+                          iconData: loginStore.passwordVisible ? Icons.visibility_off : Icons.visibility,
+                          onTap: loginStore.isPasswordVisible,
                         ),
+                      );
+                    },
+                  ),
+
+                  const SizedBox(
+                    height: 16,
+                  ),
+                  Observer(builder: (_) {
+                    return SizedBox(
+                      height: 44,
+                      child: RaisedButton(
+
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(32),
+                        ),
+
+                        child: loginStore.loading ? CircularProgressIndicator(
+                          valueColor: AlwaysStoppedAnimation(Colors.white),
+                        ) : Text('Login'),
+
+                        color: Theme.of(context).primaryColor,
+                        disabledColor: Theme.of(context).primaryColor.withAlpha(100),
+                        textColor: Colors.white,
+
+                        onPressed: loginStore.loginPressed
                       ),
-                      const SizedBox(
-                          height: 16,
-                        ),
-
-
-                        Observer(builder: (_) {
-                          return SizedBox(
-                            height: 44,
-                            child: RaisedButton(
-                              shape: RoundedRectangleBorder(
-                                borderRadius: BorderRadius.circular(32),
-                              ),
-                              child: Text('Login'),
-                              color: Theme.of(context).primaryColor,
-                              disabledColor:
-                              Theme.of(context).primaryColor.withAlpha(100),
-                              textColor: Colors.white,
-                              onPressed: loginStore.isFormValid ? () {
-                                Navigator.of(context).pushReplacement(
-                                  MaterialPageRoute(
-                                    builder: (context) => ListScreen()));
-                              } :
-                              null,
-                            ),
-                          );
-                        }),
-                  ],
-                ),
+                    );
+                  }),
+                ],
+              ),
             )),
       ),
     );
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    disposer();
   }
 }
--- todo_mobx/lib/stores/login_store.dart
+++ todo_mobx/lib/stores/login_store.dart
@@ -9,8 +9,6 @@ abstract class _LoginStore with Store {
   _LoginStore(){
     autorun((_) {
       print(email);
-      print(password);
-      print(isFormValid);
     });
   }
 
@@ -26,9 +24,28 @@ abstract class _LoginStore with Store {
   @action
   void setPassword( String value ) => password = value;
 
-  //Vamos combinar o estado desses dois observables
-  // @computed
-  // bool get isFormValid => email.length > 6 && password.length > 6;
+  @observable
+  bool passwordVisible = false;
+
+  @observable
+  bool loading = false;
+
+  @observable
+  bool loggedIn = false;
+
+  @action
+  Future<void> login() async {
+    loading = true;
+
+    // process data
+    await Future.delayed( Duration( seconds: 2 ) );
+
+    loading = false;
+    loggedIn = true;
+  }
+
+  @action
+  void isPasswordVisible() => passwordVisible = !passwordVisible;
 
   @computed
   bool get isPasswordValid => password.length >= 6;
@@ -38,4 +55,9 @@ abstract class _LoginStore with Store {
 
   @computed
   bool get isFormValid => isPasswordValid && isEmailValid;
+
+  @computed
+  Function get loginPressed => 
+    ( isEmailValid && isPasswordValid && !loading ) ? login : null;
+
 }
