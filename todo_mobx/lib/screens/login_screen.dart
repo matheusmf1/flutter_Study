6cee1060e1d432bf6e252b444d90c349ee219137
Matheus Mandotti <mathew.mfranco@gmail.com>
2020-08-16 17:00:38 -0300

    Implement form validation to enable login button

--- todo_mobx/lib/screens/login_screen.dart
+++ todo_mobx/lib/screens/login_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_mobx/flutter_mobx.dart';
 import 'package:todomobx/widgets/custom_icon_button.dart';
 import 'package:todomobx/widgets/custom_text_field.dart';
 
@@ -6,84 +7,79 @@ import '../stores/login_store.dart';
 import 'list_screen.dart';
 
 class LoginScreen extends StatefulWidget {
-
   @override
   _LoginScreenState createState() => _LoginScreenState();
 }
 
-class _LoginScreenState extends State<LoginScreen> {
-
+class _LoginScreenState extends State < LoginScreen > {
   LoginStore loginStore = LoginStore();
-
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-
       body: Container(
-        
         alignment: Alignment.center,
         margin: const EdgeInsets.all(32),
-
-        child: Card(
+          child: Card(
             shape: RoundedRectangleBorder(
               borderRadius: BorderRadius.circular(16),
             ),
             elevation: 16,
             child: Padding(
               padding: const EdgeInsets.all(16),
-              child: Column(
- 
-                mainAxisSize: MainAxisSize.min,
-                children: <Widget>[
-
-
-                  CustomTextField(
-                    hint: 'E-mail',
-                    prefix: Icon(Icons.account_circle),
-                    textInputType: TextInputType.emailAddress,
-                    onChanged: loginStore.setEmail,
-                    enabled: true,
-
-                  ),
-                  const SizedBox(
-                    height: 16,
-                  ),
-
-                  CustomTextField(
-                    hint: 'Senha',
-                    prefix: Icon(Icons.lock),
-                    obscure: true,
-                    onChanged: loginStore.setPassword,
-                    enabled: true,
-                    suffix: CustomIconButton(
-                      radius: 32,
-                      iconData: Icons.visibility,
-                      onTap: () {},
+                child: Column(
+                  mainAxisSize: MainAxisSize.min,
+                  children: < Widget > [
+                    CustomTextField(
+                      hint: 'E-mail',
+                      prefix: Icon(Icons.account_circle),
+                      textInputType: TextInputType.emailAddress,
+                      onChanged: loginStore.setEmail,
+                      enabled: true,
                     ),
-                  ),
-                  const SizedBox(
-                    height: 16,
-                  ),
-                  SizedBox(
-                    height: 44,
-                    child: RaisedButton(
-                      shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(32),
+                    const SizedBox(
+                        height: 16,
                       ),
-                      child: Text('Login'),
-                      color: Theme.of(context).primaryColor,
-                      disabledColor:
-                          Theme.of(context).primaryColor.withAlpha(100),
-                      textColor: Colors.white,
-                      onPressed: () {
-                        Navigator.of(context).pushReplacement(MaterialPageRoute(
-                            builder: (context) => ListScreen()));
-                      },
-                    ),
-                  )
-                ],
-              ),
+                      CustomTextField(
+                        hint: 'Senha',
+                        prefix: Icon(Icons.lock),
+                        obscure: true,
+                        onChanged: loginStore.setPassword,
+                        enabled: true,
+                        suffix: CustomIconButton(
+                          radius: 32,
+                          iconData: Icons.visibility,
+                          onTap: () {},
+                        ),
+                      ),
+                      const SizedBox(
+                          height: 16,
+                        ),
+
+
+                        Observer(builder: (_) {
+                          return SizedBox(
+                            height: 44,
+                            child: RaisedButton(
+                              shape: RoundedRectangleBorder(
+                                borderRadius: BorderRadius.circular(32),
+                              ),
+                              child: Text('Login'),
+                              color: Theme.of(context).primaryColor,
+                              disabledColor:
+                              Theme.of(context).primaryColor.withAlpha(100),
+                              textColor: Colors.white,
+                              onPressed: loginStore.isFormValid ? () {
+                                Navigator.of(context).pushReplacement(
+                                  MaterialPageRoute(
+                                    builder: (context) => ListScreen()));
+                              } :
+                              null,
+                            ),
+                          );
+                        }),
+                  ],
+                ),
             )),
       ),
     );
--- todo_mobx/lib/stores/login_store.dart
+++ todo_mobx/lib/stores/login_store.dart
@@ -10,6 +10,7 @@ abstract class _LoginStore with Store {
     autorun((_) {
       print(email);
       print(password);
+      print(isFormValid);
     });
   }
 
@@ -24,4 +25,17 @@ abstract class _LoginStore with Store {
 
   @action
   void setPassword( String value ) => password = value;
+
+  //Vamos combinar o estado desses dois observables
+  // @computed
+  // bool get isFormValid => email.length > 6 && password.length > 6;
+
+  @computed
+  bool get isPasswordValid => password.length >= 6;
+
+  @computed
+  bool get isEmailValid => RegExp(r"^[a-zA-Z0-9.a-zA-Z0-9.!#$%&'*+-/=?^_`{|}~]+@[a-zA-Z0-9]+\.[a-zA-Z]+").hasMatch(email);
+
+  @computed
+  bool get isFormValid => isPasswordValid && isEmailValid;
 }
