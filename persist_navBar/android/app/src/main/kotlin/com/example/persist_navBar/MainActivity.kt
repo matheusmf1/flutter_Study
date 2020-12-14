53a21500dc603413355789901c2894ccd6fcc9c8
Matheus <mathew.mfranco@gmail.com>
2020-12-14 16:13:16 -0300

    Update files

---
+++ persist_navBar/android/app/src/main/kotlin/com/example/persist_navBar/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.persist_navBar
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ persist_navBar/lib/screens.dart
@@ -0,0 +1,69 @@
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({Key key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: MaterialButton(
+        color: Colors.lightBlue,
+        child: Text("Open Secondary page"),
+        onPressed: () {
+          Navigator.of(context).push(
+            MaterialPageRoute(
+              builder: (context) => NewPage(),
+            ),
+          );
+        },
+      ),
+    );
+  }
+}
+
+class SettingsScreen extends StatelessWidget {
+  final Function openSettings;
+  const SettingsScreen({Key key, this.openSettings}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: FlatButton(
+        child: Text("Push other Settings"),
+        onPressed: () {
+          Navigator.of(context).push(
+            MaterialPageRoute(
+              builder: (context) => NewPage(),
+            ),
+          );
+        },
+      ),
+    );
+  }
+}
+
+class NewPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(
+          'New Screen',
+        ),
+      ),
+      body: Container(
+        color: Colors.lightGreen,
+        child: FlatButton(
+          child: Text("Push new Screen"),
+          onPressed: () {
+            Navigator.of(context).push(
+              MaterialPageRoute(
+                builder: (context) => NewPage(),
+              ),
+            );
+          },
+        ),
+      ),
+    );
+  }
+}
